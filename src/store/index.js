9d1de23515f43358d792daef7b1cc395ebf54815
nurcancurebal <nurcancurebal@hotmail.com>
2023-05-30 00:37:32 +0300

    router-vuex

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,13 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { productList } from '@/assets/productData'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    card: [],
+    products: productList,
   },
   getters: {
   },
