59a5c40c7135c06ca4f70fd18de0c8dccd4a28b7
nurcancurebal <nurcancurebal@hotmail.com>
2023-05-31 00:07:20 +0300

    .

---
+++ src/components/Card.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <h2>Card</h2>
+    <div></div>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+}
+
+li {
+  display: flex;
+  max-width: 700px;
+  margin: 10px auto auto;
+}
+
+.item-name {
+  text-align: left;
+  font-size: 18px;
+  flex: 2;
+}
+
+.item-count {
+  font-size: 25px;
+  margin-left: 10px;
+  flex: 2;
+}
+
+.item-amount {
+  text-align: left;
+  flex: 1;
+}
+
+.total-amount {
+  border-top: 2px solid #333;
+  border-bottom: 2px solid #333;
+  padding: 10px 20% 10px 10px;
+  margin: 10px;
+  font-size: 29px;
+  text-align: right;
+}
+
+button {
+  border: none;
+  cursor: pointer;
+  padding: 5px 10px;
+}
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -12,6 +12,14 @@ export default new Vuex.Store({
   getters: {
   },
   mutations: {
+    addToCard({ card }, product) {
+      const itemIndex = card.findIndex(item => item.id === product.id)
+      if (itemIndex === -1) {
+        card.push(product);
+      } else {
+        card[itemIndex].count = parseInt(product.count) + parseInt(card[itemIndex].count)
+      }
+    }
   },
   actions: {
   },
